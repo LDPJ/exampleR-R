0b22079fcd9878ba3ac1f441d7c4d65528134f56
Nah.Myself( ) <larrypaulingjr@gmail.com>
2017-08-21 22:30:43 -0400

    Update example.js

--- example.js
+++ example.js
@@ -1,1 +1,22 @@
-
+$(function() {
+  
+  var times;
+  $.ajax({
+    beforeSend: function(xhr){
+      if(xhr.overrideMimeType) {
+        xhr.overrideMimeTpe("application/json");
+      }
+    }
+  });
+  
+  //
+  function loadTimetable() {
+    $.getJSON('data/example.json')
+    .done( function(data){
+      times = data;
+    }).fail( function() {
+      $('#event').html('Aye! We could not load the timetable at the moment');
+    });
+  }
+  
+  loadTimetable();
